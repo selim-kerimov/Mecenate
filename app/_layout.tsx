92102a7adb3108db46334111914a0a77e8e7d438
selim-kerimov <selimkerimovdv@gmail.com>
2026-04-15 03:23:27 +0500

    fix: set status bar style dark on android

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,7 @@
 import { TanstackQueryProvider } from '@/app/providers/TanstackQueryProvider'
 import { Stack } from 'expo-router'
+import { StatusBar } from 'expo-status-bar'
+import { Platform } from 'react-native'
 import { GestureHandlerRootView } from 'react-native-gesture-handler'
 import { KeyboardProvider } from 'react-native-keyboard-controller'
 
@@ -8,6 +10,7 @@ export default function RootLayout() {
     <GestureHandlerRootView>
       <KeyboardProvider>
         <TanstackQueryProvider>
+          {Platform.OS === 'android' && <StatusBar style="dark" translucent />}
           <Stack screenOptions={{ headerShown: false }} />
         </TanstackQueryProvider>
       </KeyboardProvider>
